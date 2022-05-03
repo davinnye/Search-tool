da1188265662ffd6cf0f8027b9dc3746a9cbf84a
Vinicius 'Davina' Ferreira <viniciusferreira0708@gmail.com>
2022-05-03 16:33:10 -0300

    Styles made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-
+import {FiSearch} from 'react-icons/fi';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Teste buscador</h1>
+    <div className="container">
+      <h1 className='title'>Find your ZIP Code/CEP</h1>
+      
+      <div classname="containerInput">
+        <input type="text"
+        placeholder="Enter your ZIP Code/CEP"/>
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+
+        <main className='main'>
+          <h2>CEP: 40800470</h2>
+          <span>Rua 8 de Dezembro</span>
+          <span>Complemento: Complementando</span>
+          <span>Bairro: Paripe</span>
+          <span>Salvador - Bahia</span>
+        </main>
+      
+      
     </div>
   );
 }
